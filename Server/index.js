2b946555c2b32002e28d15db7dce685fc36a2abe
Timmy-B <timmybofficialyt@gmail.com>
2020-04-20 03:51:43 +0100

    added config

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,68 @@
-import express from 'express';
-import fs from 'fs';
+const express = require('express');
+const fs = require('fs');
+const ini = require('ini');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const ip = require('ip');
+const myIP = ip.address();
+var config = {};
+const iniFile = './config.ini'
+const defaultConfig = {
+    general: { name: 'My Bonsai', species: 'Giant sequoia', unitSystem: 'metric', firstRun: true, configVersion: 1, },
+    network: { 
+        port: 8686, 
+        uniqueKey: '', //Key true idenifier of this device. should be generated on first run. allows for name change without data loss.
+        serverIP: '', //IP of the server this device will report to.
+        mode: 'master', // master, slave - slave will not log historical data.
+    },
+    sensorCalibration: { 
+        lowTemp: 0,
+        highTemp: 32,
+        lowMoisture: 0,
+        highMoisture: 100,
+        lowHumidity : 0,
+        highHumidity: 100,
+        lowLux: 0,
+        highLux: 100
+    },
+    sensorTargets: {
+        targetTemp: 24,
+        targetMoisture: 80,
+        targetHumidity: 80,
+        targetLux: 80
+    }
+};
+try {
+    if (fs.existsSync(iniFile)) {
+        config = ini.parse(fs.readFileSync(iniFile, 'utf-8'));
+    }else{
+       const newINI = ini.stringify(defaultConfig)
+       config = defaultConfig;
+        fs.writeFileSync(iniFile, ini.stringify(defaultConfig), function (err) {
+            if (err) throw err;
+            console.log('BonsaiPi config created successfully.');
+        }); 
+    }
+} catch (err) {
+    console.error(err);
+}
+
+const port = config.network.port;
+
+app.listen(port, () => console.log(`BonasiPi listening at http://${myIP}:${port}`))
+
+app.get("/", function (req, res) {
+    res.json(config.general);
+});
+
+app.get("/", function (req, res) {
+    res.json(config.general);
+});
+
+app.route('/config')
+    .get(function (req, res) {
+        res.json(defaultConfig)
+    })
+    .put(function (req, res) {
+
+        res.send(`Updated Config - ${req.params}`)
+    })
